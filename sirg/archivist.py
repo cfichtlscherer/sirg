d13a7e4abce977131a4932c6b740b32a49181cfd
chris1218-de <chris1218@gmx.de>
2020-02-17 18:56:27 +0100

    archivist not completly finished

--- sirg/archivist.py
+++ sirg/archivist.py
@@ -4,9 +4,38 @@ Christopher Fichtlscherer (fichtlscherer@mailbox.org)
 GNU General Public License
 """
 
-import git
+import git as git
+import time as time
 
-repo = git.Repo(search_parent_directories=True)
-sha = repo.head.object.hexsha
 
-print(sha)
+
+def archivist(function):
+    def wrapper(*args, **kwargs):
+        
+        results = function(*args, **kwargs)
+        
+        repo = git.Repo(search_parent_directories=True)
+        sha = repo.head.object.hexsha[:10]
+
+        time_now = time.strftime("%H:%M:%S-%d%m%y_")
+
+        name = function.__name__
+
+        args = str(args).replace(" ", "")
+
+        file_name = time_now + sha + "_" + name + args
+    
+        print(file_name)
+    
+        # datei öffnen mit dem namen
+        # results da rein packen
+        # datei schließen 
+        # datei an ort packen wo gut
+        
+    return wrapper
+
+@archivist
+def add(a, b):
+    return a + b
+
+add(1, 2)
